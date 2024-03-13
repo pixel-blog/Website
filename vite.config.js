2e3864ccea7ae55be0940e3dc4fc633c91c0e91d
Jaron <jaron_zijlstra@outlook.com>
2024-03-13 19:37:33 +0100

    update config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicDir: process.env.NODE_ENV === "production" ? "/Website/" : "/",
   plugins: [
     vue(),
   ],
